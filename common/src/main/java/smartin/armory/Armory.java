9ba5fed594e92da98e95dd189845a6cc6bed9a32
Smartin <smartin_b@web.de>
2023-10-27 21:37:38 +0200

    init

--- common/src/main/java/smartin/armory/Armory.java
+++ common/src/main/java/smartin/armory/Armory.java
@@ -1,10 +1,14 @@
 package smartin.armory;
 
+import dev.architectury.event.events.common.LifecycleEvent;
+
 public class Armory
 {
 	public static final String MOD_ID = "armory";
 
 	public static void init() {
-		
+		LifecycleEvent.SETUP.register(()->{
+			new GenerateArmorModularConverter();
+		});
 	}
 }
