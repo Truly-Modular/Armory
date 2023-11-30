52338474c0d2d5532c1372b5ed80fdeb89059ecf
Smartin <smartin_b@web.de>
2023-11-30 20:10:56 +0100

    - armory refactor

--- common/src/main/resources/assets/miapi/models/item/armor/model/armorcopy.js
+++ common/src/main/resources/assets/miapi/models/item/armor/model/armorcopy.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 const sourceFolder = './' // Replace with the path to your source folder
-const searchString = 'default' // Replace with the string you want to replace
-const replaceString = 'chain' // Replace with the string you want to use as a replacement
+const searchString = 'base' // Replace with the string you want to replace
+const replaceString = 'heavy' // Replace with the string you want to use as a replacement
 const requiredPath = `base`
 
 function processFolder(folderPath) {
@@ -20,10 +20,37 @@ function processFolder(folderPath) {
 			// Process JSON files named "default.json"
 			const data = fs.readFileSync(filePath, 'utf8')
 			const updatedData = data.replace(new RegExp(searchString, 'g'), replaceString)
-			const newFilePath = filePath.replace('default.json', `${replaceString}.json`)
+			let newFilePath = ''
+			if (true) {
+				newFilePath = newFilePath.replace(new RegExp(searchString, 'g'), replaceString)
+				createFolderIfNotExists(newFilePath)
+			} else {
+				newFilePath = filePath.replace('default.json', `${replaceString}.json`)
+			}
 			fs.writeFileSync(newFilePath, updatedData, 'utf8')
 		}
 	})
+}
+
+function createFolderIfNotExists(path) {
+	const folders = path.split('/') // Assuming a forward slash as the separator
+	let currentPath = ''
+
+	for (const folder of folders) {
+		currentPath += folder + '/'
+
+		if (!fs.existsSync(currentPath)) {
+			try {
+				fs.mkdirSync(currentPath)
+				console.log(`Folder created at ${currentPath}`)
+			} catch (error) {
+				console.error(`Error creating folder at ${currentPath}: ${error.message}`)
+				break // Stop the loop if an error occurs
+			}
+		} else {
+			console.log(`Folder already exists at ${currentPath}`)
+		}
+	}
 }
 
 // Ensure the destination folder exists
