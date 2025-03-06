840e27a6fee04ed9cbd5765dba00509ad6be2a55
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2025-03-06 04:45:57 +0100

    - addons full 1.21, many many many fixes

--- armory-forge/src/main/java/smartin/armory/forge/ArmoryForge.java
+++ armory-forge/src/main/java/smartin/armory/forge/ArmoryForge.java
@@ -1,15 +1,12 @@
 package smartin.armory.forge;
 
-import dev.architectury.platform.forge.EventBuses;
+
+import net.neoforged.fml.common.Mod;
 import smartin.armory.Armory;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Armory.MOD_ID)
 public class ArmoryForge {
     public ArmoryForge() {
-		// Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(Armory.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-            Armory.init();
+        Armory.init();
     }
 }
