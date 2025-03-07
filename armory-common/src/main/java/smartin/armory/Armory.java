7aabc935eebe16926cb54b47da9b65fa5aea76a2
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2025-03-07 23:05:38 +0100

    - fixed neo

--- armory-common/src/main/java/smartin/armory/Armory.java
+++ armory-common/src/main/java/smartin/armory/Armory.java
@@ -1,12 +1,16 @@
 package smartin.armory;
 
-import dev.architectury.event.events.common.LifecycleEvent;
+import dev.architectury.event.EventResult;
+import smartin.miapi.events.MiapiEvents;
 
 public class Armory
 {
 	public static final String MOD_ID = "tm_armory";
 
 	public static void init() {
-		LifecycleEvent.SETUP.register(()-> new GenerateArmorModularConverter());
+		MiapiEvents.GENERATE_MATERIAL_CONVERTERS.register((material, tools, armorItems, isClient) -> {
+			GenerateConvertersHelperArmor.setup(armorItems, material);
+			return EventResult.pass();
+		});
 	}
 }
