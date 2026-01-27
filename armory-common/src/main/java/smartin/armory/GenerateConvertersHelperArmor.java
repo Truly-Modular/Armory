52e8e4eed449a4b7ac1b820c91e66e926b358c63
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2026-01-27 07:17:54 +0100

    - improve stat display - fix generated material issues

--- armory-common/src/main/java/smartin/armory/GenerateConvertersHelperArmor.java
+++ armory-common/src/main/java/smartin/armory/GenerateConvertersHelperArmor.java
@@ -13,164 +13,164 @@ import smartin.miapi.modules.ModuleInstance;
 import smartin.miapi.modules.properties.ItemIdProperty;
 import smartin.miapi.registries.RegistryInventory;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class GenerateConvertersHelperArmor {
 
     public static void setup(List<ArmorItem> armorItems, Material material) {
-        // Helmet
         if (armorItems.isEmpty()) {
+            Miapi.LOGGER.error("Armor setup aborted: armorItems list is empty.");
             return;
         }
+
         ArmorItem armorItem = armorItems.getFirst();
-        if (armorItem != null && material instanceof GeneratedMaterial material1) {
-            material1.stats.put("toughness", (double) armorItem.getMaterial().value().toughness());
+        if (armorItem == null) {
+            Miapi.LOGGER.error("Armor setup aborted: first ArmorItem is null.");
+            return;
         }
 
-        armorItems.stream()
-                .filter(a -> a.getEquipmentSlot() == EquipmentSlot.HEAD)
-                .map(a -> (Item) a)
-                .findFirst()
-                .ifPresent(item -> {
-                    ModularItemStackConverter.converters.add(stack -> {
-                        if (stack.getItem().equals(item)) {
-                            return helmetItem(material);
-                        }
-                        return stack;
-                    });
-                });
+        if (!(material instanceof GeneratedMaterial generatedMaterial)) {
+            Miapi.LOGGER.error(
+                    "Armor setup aborted: material is not a GeneratedMaterial (material = {}).",
+                    material
+            );
+            return;
+        }
 
-        // Chestplate
-        armorItems.stream()
-                .filter(a -> a.getEquipmentSlot() == EquipmentSlot.CHEST)
-                .map(a -> (Item) a)
-                .findFirst()
-                .ifPresent(item -> {
-                    ModularItemStackConverter.converters.add(stack -> {
-                        if (stack.getItem().equals(item)) {
-                            return chestplateItem(material);
-                        }
-                        return stack;
-                    });
-                });
+        // At this point we are guaranteed to reach toughness logic
+        generatedMaterial.stats.put(
+                "toughness",
+                (double) armorItem.getMaterial().value().toughness()
+        );
 
-        // Leggings
-        armorItems.stream()
-                .filter(a -> a.getEquipmentSlot() == EquipmentSlot.LEGS)
-                .map(a -> (Item) a)
-                .findFirst()
-                .ifPresent(item -> {
-                    ModularItemStackConverter.converters.add(stack -> {
-                        if (stack.getItem().equals(item)) {
-                            return leggingsItem(material);
-                        }
-                        return stack;
-                    });
-                });
+        if (armorItems.size() == 4) {
+            double totalArmor = armorItems.stream()
+                    .collect(Collectors.summarizingInt(ArmorItem::getDefense))
+                    .getSum();
 
-        // Boots
-        armorItems.stream()
-                .filter(a -> a.getEquipmentSlot() == EquipmentSlot.FEET)
-                .map(a -> (Item) a)
-                .findFirst()
-                .ifPresent(item -> {
-                    ModularItemStackConverter.converters.add(stack -> {
-                        if (stack.getItem().equals(item)) {
-                            return bootsItem(material);
-                        }
-                        return stack;
-                    });
-                });
+            double desiredHardness =
+                    (totalArmor
+                     + (generatedMaterial.stats.get("flexibility") / 4)
+                     + (generatedMaterial.stats.get("density") / 4)
+                     - 1
+                    ) / 4.05;
+
+            double max = Math.max(totalArmor, desiredHardness);
+
+            if (!(Math.abs(totalArmor - desiredHardness) <= (15 / 100.0) * max)) {
+                generatedMaterial.stats.put("armor_hardness", desiredHardness);
+            }
+        }
+
+        addArmorConverter(armorItems, EquipmentSlot.HEAD, GenerateConvertersHelperArmor::helmetItem, material);
+        addArmorConverter(armorItems, EquipmentSlot.CHEST, GenerateConvertersHelperArmor::chestplateItem, material);
+        addArmorConverter(armorItems, EquipmentSlot.LEGS, GenerateConvertersHelperArmor::leggingsItem, material);
+        addArmorConverter(armorItems, EquipmentSlot.FEET, GenerateConvertersHelperArmor::bootsItem, material);
+
     }
+
+    private static void addArmorConverter(
+            Collection<? extends ArmorItem> armorItems,
+            EquipmentSlot slot,
+            Function<Material, ItemStack> itemFactory,
+            Material material
+    ) {
+        armorItems.stream()
+                .filter(a -> a.getEquipmentSlot() == slot)
+                .map(a -> (Item) a)
+                .findFirst()
+                .ifPresent(item ->
+                        ModularItemStackConverter.converters.add(stack -> {
+                            if (stack.getItem().equals(item)) {
+                                return itemFactory.apply(material);
+                            }
+                            return stack;
+                        })
+                );
+    }
+
+    private static ModuleInstance module(String moduleId, Material material) {
+        ModuleInstance instance = new ModuleInstance(
+                Objects.requireNonNull(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(
+                        Miapi.id("tm_armory", moduleId)
+                ))
+        );
+        MaterialProperty.setMaterial(instance, material);
+        return instance;
+    }
+
+    private static ItemStack buildArmorItem(
+            String rootModuleId,
+            Material material,
+            Map<String, String> subModules
+    ) {
+        ModuleInstance root = new ModuleInstance(
+                Objects.requireNonNull(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(
+                        Miapi.id("tm_armory", rootModuleId)
+                ))
+        );
+
+        subModules.forEach((slot, moduleId) ->
+                root.setSubModule(slot, module(moduleId, material))
+        );
+
+        ItemStack stack = new ItemStack(RegistryInventory.modularItem);
+        root.writeToItem(stack);
+        return ItemIdProperty.changeId(stack);
+    }
+
 
 
     public static ItemStack bootsItem(Material material) {
-        // , Miapi.registryAccess
-        ModuleInstance bootsModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/boots")));
-
-        ModuleInstance leftBootModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/boot_left")));
-        MaterialProperty.setMaterial(leftBootModule, material);
-
-        ModuleInstance rightBootModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/boot_right")));
-        MaterialProperty.setMaterial(rightBootModule, material);
-
-        bootsModule.setSubModule("boot_left", leftBootModule);
-        bootsModule.setSubModule("boot_right", rightBootModule);
-
-        ItemStack bootsItem = new ItemStack(RegistryInventory.modularItem);
-        bootsModule.writeToItem(bootsItem);
-        bootsItem = ItemIdProperty.changeId(bootsItem);
-
-        return bootsItem;
+        return buildArmorItem(
+                "armor/boots",
+                material,
+                Map.of(
+                        "boot_left",  "armor/default/boot_left",
+                        "boot_right", "armor/default/boot_right"
+                )
+        );
     }
 
     public static ItemStack leggingsItem(Material material) {
-        ModuleInstance leggingsModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/pants")));
-
-        ModuleInstance beltModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/belt")));
-        MaterialProperty.setMaterial(beltModule, material);
-
-        ModuleInstance leftLegModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/leg_left")));
-        MaterialProperty.setMaterial(leftLegModule, material);
-
-        ModuleInstance rightLegModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/leg_right")));
-        MaterialProperty.setMaterial(rightLegModule, material);
-
-        leggingsModule.setSubModule("belt", beltModule);
-        leggingsModule.setSubModule("leg_left", leftLegModule);
-        leggingsModule.setSubModule("leg_right", rightLegModule);
-
-        ItemStack leggingsItem = new ItemStack(RegistryInventory.modularItem);
-        leggingsModule.writeToItem(leggingsItem);
-        leggingsItem = ItemIdProperty.changeId(leggingsItem);
-
-        return leggingsItem;
+        return buildArmorItem(
+                "armor/pants",
+                material,
+                Map.of(
+                        "belt",      "armor/default/belt",
+                        "leg_left",  "armor/default/leg_left",
+                        "leg_right", "armor/default/leg_right"
+                )
+        );
     }
+
 
     public static ItemStack chestplateItem(Material material) {
-        ModuleInstance chestplateModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/chestplate")));
-
-        ModuleInstance frontChestModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/front_chest")));
-        MaterialProperty.setMaterial(frontChestModule, material);
-
-        ModuleInstance backChestModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/back_chest")));
-        MaterialProperty.setMaterial(backChestModule, material);
-
-        ModuleInstance leftArmModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/arm_left")));
-        MaterialProperty.setMaterial(leftArmModule, material);
-
-        ModuleInstance rightArmModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/arm_right")));
-        MaterialProperty.setMaterial(rightArmModule, material);
-
-        chestplateModule.setSubModule("chest_front", frontChestModule);
-        chestplateModule.setSubModule("chest_back", backChestModule);
-        chestplateModule.setSubModule("arm_left", leftArmModule);
-        chestplateModule.setSubModule("arm_right", rightArmModule);
-
-        ItemStack chestplateItem = new ItemStack(RegistryInventory.modularItem);
-        chestplateModule.writeToItem(chestplateItem);
-        chestplateItem = ItemIdProperty.changeId(chestplateItem);
-
-        return chestplateItem;
+        return buildArmorItem(
+                "armor/chestplate",
+                material,
+                Map.of(
+                        "chest_front", "armor/default/front_chest",
+                        "chest_back",  "armor/default/back_chest",
+                        "arm_left",    "armor/default/arm_left",
+                        "arm_right",   "armor/default/arm_right"
+                )
+        );
     }
+
 
     public static ItemStack helmetItem(Material material) {
-        // Create the root module for the helmet
-        ModuleInstance helmetModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/helmet")));
-
-        // Create the "hat" submodule representing the actual helmet part
-        ModuleInstance hatModule = new ModuleInstance(RegistryInventory.ITEM_MODULE_MIAPI_REGISTRY.get(Miapi.id("tm_armory", "armor/default/helmet")));
-        MaterialProperty.setMaterial(hatModule, material);
-
-        // Set "hat" as a child of "helmet"
-        helmetModule.setSubModule("hat", hatModule);
-
-        // Create the ItemStack for the helmet with the configured module
-        ItemStack helmetItem = new ItemStack(RegistryInventory.modularItem);
-        helmetModule.writeToItem(helmetItem);
-        helmetItem = ItemIdProperty.changeId(helmetItem);
-
-        return helmetItem;
+        return buildArmorItem(
+                "armor/helmet",
+                material,
+                Map.of(
+                        "hat", "armor/default/helmet"
+                )
+        );
     }
-
 }
